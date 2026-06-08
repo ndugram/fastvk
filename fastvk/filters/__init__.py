52cec2efecafbe80dccbade834e29de11ebec865
NEFORCEO <pirogartem919@gmail.com>
2026-06-08 22:03:47 +0700

    feat: create fastvk/filters/__init__.py

---
+++ fastvk/filters/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from .base import BaseFilter
+from .builtin import Command, FromUser, IsChat, StateFilter, Text
+
+__all__ = ["BaseFilter", "Command", "FromUser", "IsChat", "StateFilter", "Text"]
