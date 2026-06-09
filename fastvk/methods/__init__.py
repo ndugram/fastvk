e60968304da4ed92cb2074da1dfe26d421bd75a5
NEFORCEO <pirogartem919@gmail.com>
2026-06-09 17:44:00 +0700

    update fastvk/methods/__init__.py

--- fastvk/methods/__init__.py
+++ fastvk/methods/__init__.py
@@ -1,32 +1,55 @@
 from .base import VKMethod
+from .docs import DocsGetMessagesUploadServer, DocsSave
 from .groups import GroupsGetById, GroupsGetLongPollServer, GroupsGetMembers
 from .messages import (
     MessagesDelete,
     MessagesEdit,
     MessagesGetById,
     MessagesGetConversations,
+    MessagesGetHistory,
     MessagesGetLongPollServer,
+    MessagesMarkAsRead,
+    MessagesPin,
     MessagesSend,
     MessagesSendEventAnswer,
     MessagesSetActivity,
+    MessagesUnpin,
 )
+from .photos import PhotosGetMessagesUploadServer, PhotosSaveMessagesPhoto
 from .users import UsersGet, UsersSearch
+from .wall import WallGetById, WallPost
 
 # registry: VK method string → typed VKMethod class
 # used by _APICallable to route dynamic calls through Pydantic
 _REGISTRY: dict[str, type[VKMethod]] = {
+    # messages
     "messages.send": MessagesSend,
     "messages.delete": MessagesDelete,
     "messages.edit": MessagesEdit,
+    "messages.pin": MessagesPin,
+    "messages.unpin": MessagesUnpin,
+    "messages.markAsRead": MessagesMarkAsRead,
+    "messages.getHistory": MessagesGetHistory,
     "messages.setActivity": MessagesSetActivity,
     "messages.getById": MessagesGetById,
     "messages.getConversations": MessagesGetConversations,
     "messages.sendMessageEventAnswer": MessagesSendEventAnswer,
+    # users
     "users.get": UsersGet,
     "users.search": UsersSearch,
+    # groups
     "groups.getLongPollServer": GroupsGetLongPollServer,
     "groups.getById": GroupsGetById,
     "groups.getMembers": GroupsGetMembers,
+    # wall
+    "wall.post": WallPost,
+    "wall.getById": WallGetById,
+    # photos
+    "photos.getMessagesUploadServer": PhotosGetMessagesUploadServer,
+    "photos.saveMessagesPhoto": PhotosSaveMessagesPhoto,
+    # docs
+    "docs.getMessagesUploadServer": DocsGetMessagesUploadServer,
+    "docs.save": DocsSave,
 }
 
 __all__ = [
@@ -36,6 +59,10 @@ __all__ = [
     "MessagesSend",
     "MessagesDelete",
     "MessagesEdit",
+    "MessagesPin",
+    "MessagesUnpin",
+    "MessagesMarkAsRead",
+    "MessagesGetHistory",
     "MessagesSetActivity",
     "MessagesGetById",
     "MessagesGetConversations",
@@ -48,4 +75,13 @@ __all__ = [
     "GroupsGetById",
     "GroupsGetLongPollServer",
     "GroupsGetMembers",
+    # wall
+    "WallPost",
+    "WallGetById",
+    # photos
+    "PhotosGetMessagesUploadServer",
+    "PhotosSaveMessagesPhoto",
+    # docs
+    "DocsGetMessagesUploadServer",
+    "DocsSave",
 ]
