3f35e8713d3f493c006f999468a6b9b78c5a7c56
NEFORCEO <pirogartem919@gmail.com>
2026-06-09 08:23:39 +0700

    update fastvk/types/__init__.py

--- fastvk/types/__init__.py
+++ fastvk/types/__init__.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
+from .callback import CallbackQuery
 from .message import Message
 from .update import Update
 from .user import User
 
-__all__ = ["Message", "Update", "User"]
+__all__ = ["CallbackQuery", "Message", "Update", "User"]
