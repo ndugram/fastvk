14c116efd1c88bcda174b790075bf1f4849909ef
NEFORCEO <pirogartem919@gmail.com>
2026-06-09 09:40:57 +0700

    update fastvk/filters/__init__.py

--- fastvk/filters/__init__.py
+++ fastvk/filters/__init__.py
@@ -2,5 +2,6 @@ from __future__ import annotations
 
 from .base import BaseFilter
 from .builtin import Command, FromUser, IsChat, StateFilter, Text
+from .magic import F, MagicFilter
 
-__all__ = ["BaseFilter", "Command", "FromUser", "IsChat", "StateFilter", "Text"]
+__all__ = ["BaseFilter", "Command", "F", "FromUser", "IsChat", "MagicFilter", "StateFilter", "Text"]
