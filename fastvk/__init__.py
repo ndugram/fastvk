643a95e22046e61fe9497cdb5d2105cdee9c7f7a
NEFORCEO <pirogartem919@gmail.com>
2026-06-09 08:23:30 +0700

    update fastvk/__init__.py

--- fastvk/__init__.py
+++ fastvk/__init__.py
@@ -3,8 +3,10 @@ from __future__ import annotations
 from .app import FastVK
 from .router import Router
 from .api.client import Bot
+from .keyboard import Button, Keyboard
+from .types.callback import CallbackQuery
 from . import filters
 from . import fsm
 from . import types
 
-__all__ = ["FastVK", "Router", "Bot", "filters", "fsm", "types"]
+__all__ = ["FastVK", "Router", "Bot", "Button", "CallbackQuery", "Keyboard", "filters", "fsm", "types"]
