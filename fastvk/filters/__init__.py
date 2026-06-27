7a413c6935a4c585078e0d091585c46c5013977b
NEFORCEO <pirogartem919@gmail.com>
2026-06-27 18:07:04 +0700

    update fastvk/filters/__init__.py

--- fastvk/filters/__init__.py
+++ fastvk/filters/__init__.py
@@ -1,14 +1,30 @@
 from __future__ import annotations
 
 from .base import BaseFilter
-from .builtin import Command, CommandHelp, CommandStart, FromUser, IsChat, StateFilter, Text
+from .builtin import (
+    CallbackDataFilter,
+    Command,
+    CommandHelp,
+    CommandStart,
+    FromUser,
+    IsChat,
+    StateFilter,
+    Text,
+)
 from .magic import F, MagicFilter
 from ..types.command import CommandArgs
 
 __all__ = [
     "BaseFilter",
-    "Command", "CommandStart", "CommandHelp", "CommandArgs",
-    "F", "MagicFilter",
-    "FromUser", "IsChat",
-    "StateFilter", "Text",
+    "Command",
+    "CommandStart",
+    "CommandHelp",
+    "CommandArgs",
+    "F",
+    "MagicFilter",
+    "FromUser",
+    "IsChat",
+    "StateFilter",
+    "Text",
+    "CallbackDataFilter",
 ]
