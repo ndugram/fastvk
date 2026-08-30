ca53c65bf81577befda38f1c1153dfb63ff087ab
NEFORCEO <pirogartem919@gmail.com>
2026-08-30 17:13:57 +0700

    test: update test_throttling.py

--- tests/test_throttling.py
+++ tests/test_throttling.py
@@ -4,7 +4,6 @@ import time
 from typing import Any
 from unittest.mock import AsyncMock
 
-import pytest
 
 from fastvk.middleware.throttling import ThrottlingMiddleware
 from fastvk.types.update import Update
